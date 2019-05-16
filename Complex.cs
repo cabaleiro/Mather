ee1a32e2f85432b49411c0524013b9119eda126c
cabaleiro <lucario606@gmail.com>
2019-05-17 00:06:53 +0200

    Improved Complex and Vector3

--- Complex.cs
+++ Complex.cs
@@ -2,45 +2,86 @@
 
 public class Complex
 {
-	public Complex(double _real, double _imag)
+	public Complex(double _real = 0, double _imag = 0)
 	{
         real = _real;
         imaginary = _imag;
         isCartesian = true;
         //Calculate radius and angle
+        makePolar();
 	}
 
-    public Complex(double _rad, double _ang, byte _isCartesian)
+    public Complex(double _rad = 0, double _ang = 0, byte _isCartesian)
     {
         radius = _rad;
         angle = _ang;
         isCartesian = false;
         //Calculate real and imaginary
+        makeCartesian();
     }
-    double real;
-    double imaginary;
-    double radius;
-    double angle;
-    byte isCartesian;
+    private double real;
+    private double imaginary;
+    private double radius;
+    private double angle;
+    private byte isCartesian;
 
     //Cartesian: a + bi
     //Polar: rcos ang + rsin ang i
     //Exponential: r e^(i angle)
-    printCartesian()
+    public static makeCartesian()
+    {
+        real = radius * Math.Cos(angle);
+        imaginary = radius * Math.Sin(angle);
+    }
+    public static makePolar()
+    {
+        radius = Math.Sqrt(real ^ 2 + imaginary ^ 2);
+        angle = Math.Atan(imaginary / real);
+    }
+    public static ToString()
+    {
+        if (isCartesian == true)
+            { return printCartesian(); }
+        else
+            { return printPolar(); }
+    }
+    public static printCartesian()
     {
         return (real.ToString() + " + " + imaginary.ToString() + "i");
     }
-    printPolar()
+    public static printPolar()
     {
-        return (radius.ToString() + " cos (" + angle.ToString() + ")" + " + " + radius.ToString() + "i " + "sin(" + angle.ToString() + ")");
+        //return (radius.ToString() + " cos (" + angle.ToString() + ")" + " + " + radius.ToString() + "i " + "sin(" + angle.ToString() + ")");
+        return (radius.ToString() + " |_ " + angle.ToString());
     }
-
-
+    public static Complex operator +(Complex _num1, Complex _num2)
+    {
+        Complex resultNum = new Complex();
+        resultNum.real = _num1.real + _num2.real;
+        resultNum.imaginary = _num1.imaginary + _num2.imaginary;
+        return resultNum;
+    }
+    public static Complex operator -(Complex _num1, Complex _num2)
+    {
+        Complex resultNum = new Complex();
+        resultNum.real = _num1.real - _num2.real;
+        resultNum.imaginary = _num1.imaginary - _num2.imaginary;
+        return resultNum;
+    }
+    public static Complex operator *(Complex _num1, Complex _num2)
+    {
+        Complex resultNum = new Complex();
+        resultNum.real = _num1.radius * _num2.radius;
+        resultNum.imaginary = _num1.angle + _num2.angle;
+        return resultNum;
+    }
+    public static Complex operator /(Complex _num1, Complex _num2)
+    {
+        Complex resultNum = new Complex();
+        resultNum.real = _num1.radius / _num2.radius;
+        resultNum.imaginary = _num1.angle - _num2.angle;
+        return resultNum;
+    }
     //add override for stdout << "real + imaginary i"
-    //add override for * template (using real and imag as binom) (or using polar)
-    //add override for + template (real + real, imag + imag)
-    //add override for - template (^^)
-    //add override for / template (multip by conjugate up and down) (or using polar)
-    //add override for ** int (using *)
     //add override for root and ^^ using DeMoivre's Theorem
 }
--- Vector3.cs
+++ Vector3.cs
@@ -4,65 +4,72 @@ public class Vector3
 {
 	public Vector3(double _x = 0, double _y = 0, double _z = 0)
 	{
-
+        x = _x;
+        y = _y;
+        z = _z;
+        calcLength();
 	}
     private double x;
     private double y;
     private double z;
     private double length;
 
-    public static Length()
+    public static calcLength()
     {
         length = Math.Sqrt(x ^ 2 + y ^ 2 + z ^ 2);
-        return length;
     }
 
-    public static Vector3 operator +(Vector3 _Vec1, Vector3 _Vec2)
+    public static ToString()
+    {
+        return "(" + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ")";
+    }
+
+    public static Vector3 operator +(Vector3 _vec1, Vector3 _vec2)
     {
         Vector3 resultVector = new Vector3();
-        resultVector.x = _Vec1.x + _Vec2.x;
-        resultVector.y = _Vec1.y + _Vec2.y;
-        resultVector.z = _Vec1.z + _Vec2.z;
+        resultVector.x = _vec1.x + _vec2.x;
+        resultVector.y = _vec1.y + _vec2.y;
+        resultVector.z = _vec1.z + _vec2.z;
         return resultVector;
     }
-    public static Vector3 operator -(Vector3 _Vec1, Vector3 _Vec2)
+    public static Vector3 operator -(Vector3 _vec1, Vector3 _vec2)
     {
         Vector3 resultVector = new Vector3();
-        resultVector.x = _Vec1.x - _Vec2.x;
-        resultVector.y = _Vec1.y - _Vec2.y;
-        resultVector.z = _Vec1.z - _Vec2.z;
+        resultVector.x = _vec1.x - _vec2.x;
+        resultVector.y = _vec1.y - _vec2.y;
+        resultVector.z = _vec1.z - _vec2.z;
         return resultVector;
     }
-    public static Vector3 operator *(Vector3 _Vec1, Vector3 _Vec2)
+    public static Vector3 operator *(Vector3 _vec1, Vector3 _vec2)
     {
         double resultVal = 0;
-        resultVal += _Vec1.x * _Vec2.x;
-        resultVal += _Vec1.y * _Vec2.y;
-        resultVal += _Vec1.z * _Vec2.z;
+        resultVal += _vec1.x * _vec2.x;
+        resultVal += _vec1.y * _vec2.y;
+        resultVal += _vec1.z * _vec2.z;
         return resultVal;
     }
-    public static Vector3 operator *(Vector3 _Vec1, double _constant)
+    public static Vector3 operator *(Vector3 _vec1, double _constant)
     {
         double resultVal = 0;
-        resultVal += _Vec1.x * _constant;
-        resultVal += _Vec1.y * _constant;
-        resultVal += _Vec1.z * _constant;
+        resultVal += _vec1.x * _constant;
+        resultVal += _vec1.y * _constant;
+        resultVal += _vec1.z * _constant;
         return resultVal;
     }
-    public static Vector3 operator *(Vector3 _Vec1, int _constant)
+    public static Vector3 operator *(Vector3 _vec1, int _constant)
     {
         double resultVal = 0;
-        resultVal += _Vec1.x * _constant;
-        resultVal += _Vec1.y * _constant;
-        resultVal += _Vec1.z * _constant;
+        resultVal += _vec1.x * _constant;
+        resultVal += _vec1.y * _constant;
+        resultVal += _vec1.z * _constant;
         return resultVal;
     }
-    public static Vector3 operator ^(Vector3 _Vec1, Vector3 _Vec2)
+    public static Vector3 operator ^(Vector3 _vec1, Vector3 _vec2)
     {
         Vector3 resultVector = new Vector3();
-        resultVector.x = (_Vec1.y * _Vec2.z) - (_Vec1.z * _Vec2.y);
-        resultVector.y = (_Vec1.x * _Vec2.z) - (_Vec1.z * _Vec2.x);
-        resultVector.z = (_Vec1.x * _Vec2.y) - (_Vec1.y * _Vec2.x);
+        resultVector.x = (_vec1.y * _vec2.z) - (_vec1.z * _vec2.y);
+        resultVector.y = (_vec1.x * _vec2.z) - (_vec1.z * _vec2.x);
+        resultVector.z = (_vec1.x * _vec2.y) - (_vec1.y * _vec2.x);
         return resultVal;
     }
 }
