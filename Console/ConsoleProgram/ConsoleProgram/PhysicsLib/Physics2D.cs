753b2acd394128327485c3d2906c644eac5d09e9
cabaleiro <lucario606@gmail.com>
2019-05-19 23:09:06 +0200

    Added base structs to Physics2D

--- Console/ConsoleProgram/ConsoleProgram/PhysicsLib/Physics2D.cs
+++ Console/ConsoleProgram/ConsoleProgram/PhysicsLib/Physics2D.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 
+public struct Particle
+{
+    Vector3 position;
+    Vector3 speed;
+    Vector3 acceleration;
+    Vector3 mass;
+}
+
+public struct Body
+{
+    Particle centerOfMass;
+    Vector3 angularPosition;
+    Vector3 angularSpeed;
+    Vector3 angularAccelation;
+    Geometry shape;
+}
+
+
 public class Physics2D
 {
 	public Physics2D()
 	{
 	}
+
+    public static double EvalPosition()
+    {
+        return 0;
+    }
 
     public static void ResultantForce()
     {
