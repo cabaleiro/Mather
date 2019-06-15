28a7d4a7b891190871693451f61ceb54712ee8a8
cabaleiro <lucario606@gmail.com>
2019-06-15 03:48:33 +0200

    Miscellaneous improvements
    - Improved Triangle class with laws and random comprobators
    - Created OwnParser

--- Console/ConsoleProgram/ConsoleProgram/Geometry/Geometry.cs
+++ Console/ConsoleProgram/ConsoleProgram/Geometry/Geometry.cs
@@ -81,18 +81,28 @@ public class Triangle
     double[] angles = { 0, 0, 0 };
     double[] sides = { 0, 0, 0 };
     bool isStraight = false;
+    byte straightSide = 0;
 
 
     //The vectors are defined in the anti-clockwise direction
     //The first side is the side that opens to the first angle, in any order
+    public Triangle(Point A, Point B, Point C)
+    {
+
+    }
     public Triangle(double ang1, double ang2, double ang3, double side1, double side2, double side3)
     {
         int counterSides = 0;
         int counterAngles = 0;
+
+        //Check if the ordered input is zero
         if (ang1 != 0)
         {
+            //Add angle to vector angles
             angles[0] = ang1;
+            //Increase the control counter
             counterAngles += 1;
+            //Check for special condition
             if (ang1 == 90)
                 isStraight = true;
 
@@ -129,32 +139,66 @@ public class Triangle
 
         //Check if two sides or two angles are equal
         //Check if angle sum >180
+        if (AngleSum() > 180)
+        {
+            //Call delete
+        }
         //If counter of non-zero values is > 3, check if values are compatible
 
         //
     }
+
+    public void IsStraight()
+    {
+        if (angles[0] == 90)
+        {
+            isStraight = true;
+            straightSide = 0;
+            return;
+        }
+        if (angles[1] == 90)
+        {
+            isStraight = true;
+            straightSide = 1;
+            return;
+        }
+        if (angles[2] == 90)
+        {
+            isStraight = true;
+            straightSide = 2;
+            return;
+        }
+    }
+    public double AngleSum()
+    {
+        return angles[0] + angles [1] + angles [2];
+    }
+
     public double SineLawSide(double knownSide, double knownAngle, double unknownAngle)
     {
         //a = sinA * b / sinB
-        return 0;
+        return Math.Sin(unknownAngle)*knownSide/Math.Sin(knownAngle);
     }
     public double SineLawAngle(double knownAngle, double knownSide, double unknownSide)
     {
         //sinA = a * sinB / b
-        return 0;
+        return Math.Asin(unknownSide)*Math.Sin(knownAngle)/knownSide;
     }
-    public double CosineLawSide()
+    public double CosineLawSide(double sideB, double sideC, double ownAngle)
     {
         //a^2 = b^2 + c^2 -2bc cosA
-        return 0;
+        return Math.Sqrt(Math.Pow(sideB, 2) + Math.Pow(sideC, 2) - 2*sideB*sideC*Math.Cos(ownAngle));
     }
-    public double CosineLawAngle()
+    public double CosineLawAngle(double ownSide, double sideA, double sideB)
     {
         //cosA = (b^2 + c^2 - a^2)/(2bc)
-        return 0;
+        return Math.Acos(Math.Pow(sideA, 2) + Math.Pow(sideB, 2) - Math.Pow(ownSide, 2) / (2*sideA*sideB));
     }
     public double Area()
-    {   
+    {
+        double area = 0;
+        double s = (sides[0] + sides[1] + sides[2]) * 0.5 ;
+        area = Math.Sqrt(s*(s-sides[0])*(s-sides[1])*(s-sides[2]));
         //if angle != 90
         //Area = sqrt(s*(s-a)(s-b)(s-c))
         //if angle = 90
---
+++ Console/ConsoleProgram/ConsoleProgram/MultiLib/OwnParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleProgram.MultiLib
+{
+    class OwnParser
+    {
+        //Hard formatting input for this parser (Implies needing a proxy class)
+        // y=bx*2*0.58*e^(x+2)-5
+            //If no = sign is detected and no variables, operate
+            //Already detects y and x as variables
+            // y= is an idiom, calls function parser
+            //b isn't a variable or number, goes to PARAMETER list
+                //PARAMETER b
+                //BIN *
+                //VARIABLE x
+                //BIN *
+                //CONST 2
+                //BIN *
+                //CONST 0.58
+                //BIN *
+                //SPECIAL CONST e
+                //BIN ^
+                // WAIT
+                //VARIABLE x
+                //BIN +
+                //CONST 2
+                // END WAIT
+                //BIN ^
+                //RESULT
+                // -
+                // 5
+        //MAINTHREAD:
+            //TOKEN 1 (X+5) TO SUBTHREAD 1
+                //TOKEN 1 = [token struct]
+            //TOKEN 2 (2X/7) TO SUBTHREAD 2
+            //TOKEN 3 (3b+2) TO SUBTHREAD 3
+            
+
+    }
+}
